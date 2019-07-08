9eb659220287a6f014d406d8ed436080d0ae6582
goukehao <727999218@qq.com>
2019-07-08 11:00:22 +0800

    自己玩的

---
+++ vue-cli/src/components/HelloWorld.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    dfsfdsf
+  </div>
+</template>
+
+<script>
+import Fonte from './Fonte.vue';
+export default {
+  components:{Fonte},
+  name:'HelloWorld',
+  data() {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    };
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
--- webpack/build/webpack.dev.js
+++ webpack/build/webpack.dev.js
@@ -27,13 +27,7 @@ module.exports={
           },
         ]
       },
-    resolve:{
-        extensions:['.js','.vue','.json'],
-        alias:{
-            'vue$':'vue/dist/vue.esm.js',
-            '@':resolve('src')
-        }
-    },
+   
     devServer:{
         contentBase:path.resolve(__dirname,'../dist'), //网站的根目录为 根目录/dist，如果配置不对，会报Cannot GET /错误
         port:9000,
